9a2dc2506b9e1c82ee1856e1c1b82d7a0557ff15
plu7o <skankhunt4ort2wo@gmail.com>
2023-09-25 15:53:30 +0200

    setup freestanding rust binary

--- rick_os/src/main.rs
+++ rick_os/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point
+    loop {}
+}
+
+// This function is called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
